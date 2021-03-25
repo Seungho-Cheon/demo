e831f85ce401233a9b07aecd193d087126313ed7
Seungho-Cheon <jongroinf@naver.com>
2021-03-25 16:16:10 +0900

    첫번째 업데이트

---
+++ demo/src/com/demo/hello/Hello.java
@@ -0,0 +1,13 @@
+/* This is a simple Java program. 
+   FileName : "Hello.java".       */
+
+package com.demo.hello;
+
+public class Hello {
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World~^.^"); 
+    } 
+}
